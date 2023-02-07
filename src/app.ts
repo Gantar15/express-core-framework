08f529261b990c2aa3cd1a95db4bb59ba4ff56d0
Gantar15 <pavlovskiy.egor@inbox.ru>
2023-02-07 21:45:57 +0300

    init

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express, { Express } from "express";
+import { Server } from "http";
+import { UserController } from "./users/users.controller";
+import { ILogger } from "./logger/logger.interface";
+import { inject, injectable } from "inversify";
+import { TYPES } from "./types";
+import { IExeptionFilter } from "./errors/exception.filter.interface";
+import "reflect-metadata";
+
+@injectable()
+export class App {
+	app: Express;
+	server: Server;
+	port: number;
+
+	constructor(
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.IUserController) private userController: UserController,
+		@inject(TYPES.IExeptionFilter) private exceptionFilter: IExeptionFilter,
+	) {
+		this.app = express();
+		this.port = 8000;
+	}
+
+	useRoutes(): void {
+		this.app.use("/users", this.userController.router);
+	}
+
+	useExeptionFilters(): void {
+		this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+	}
+
+	public init(): void {
+		this.useRoutes();
+		this.useExeptionFilters();
+		this.server = this.app.listen(this.port);
+		this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
+	}
+}
---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { Container, ContainerModule } from "inversify";
+import { App } from "./app";
+import { ExceptionFilter } from "./errors/exception.filter";
+import { IExeptionFilter } from "./errors/exception.filter.interface";
+import { ILogger } from "./logger/logger.interface";
+import { LoggerService } from "./logger/logger.service";
+import { TYPES } from "./types";
+import { UserController } from "./users/users.controller";
+import { IUserController } from "./users/users.controller.interface";
+
+interface IBootsrtapReturn {
+	app: App;
+	appContainer: Container;
+}
+
+export const appBindings = new ContainerModule((bind) => {
+	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<IExeptionFilter>(TYPES.IExeptionFilter).to(ExceptionFilter);
+	bind<IUserController>(TYPES.IUserController).to(UserController);
+	bind<App>(TYPES.Application).to(App);
+});
+
+function bootstrap(): IBootsrtapReturn {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
+	return { app, appContainer };
+}
+
+export const { app, appContainer } = bootstrap();
